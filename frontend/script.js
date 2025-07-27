1e73ea4161eeec7cd7d23f272eeb50c62930d98b
Himanshu Singh <himanshusingh230103@gmail.com>
2025-07-27 18:51:18 +0530

    frontend deploy

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ async function checkNews() {
     return;
   }
 
-  const response = await fetch("http://localhost:5000/predict", {
+  const response = await fetch("https://newsverify-puch.onrender.com/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
